f8a7e0adcaa4682cc72267113cd3b96d4ae1b13f
PatrykPietruszewski <147393955+PatrykPietruszewski@users.noreply.github.com>
2024-03-26 08:21:35 +0100

    Update Zad3.c

--- Zadania2/Zad3.c
+++ Zadania2/Zad3.c
@@ -1,49 +1,61 @@
 #include <stdio.h>
-#include <math.h>
-typedef struct{
+typedef struct
+{
     int x;
     int y;
 }Point;
-
-void Zmien(Point *p){
-    
-    p->x++;
-    p->y--;
-}
-
-
-typedef struct{
-    Point *p1;
-    Point *p2;
-    Point *p3;
-    Point *p4;
+typedef struct
+{
+    Point* p1;
+    Point* p2;
+    Point* p3;
+    Point* p4;
 }Rectangle;
-
-void Pole_Obwod(Rectangle *r){
-    
-    int dl = (sqrt(pow((*r).p2->x-(*r).p1->x,2)+pow((*r).p2->y-(*r).p1->y,2)));
-    int wys = (sqrt(pow((*r).p4->x-(*r).p3->x,2)+pow((*r).p4->y-(*r).p3->y,2)));
-    
-    printf("Pole tego prostokąta to %d,\n", dl* wys);
-    printf("Obwód tego prostokąta to %d", 2*dl + 2*wys);
+void move(Point* p)
+{
+    (*p).x++;
+    (*p).y++;
 }
-
+double sqrt(double x)
+{
+    int temp=0;
+    double sqrt=x/2;
+    while(sqrt!=temp)
+    {
+        temp=sqrt;
+        sqrt=(x/temp+temp)/2;
+    }
+    return sqrt;
+}
+double pow(double x,int y)
+{
+    int i=1;
+    while(i<y)
+    {
+        x*=x;
+        i++;
+    }
+    return x;
+}
+void areaAndCimcunference(Rectangle* r)
+{
+    double length =sqrt(pow((*r).p2->x - (*r).p1->x, 2) + pow((*r).p2->y - (*r).p1->y, 2));
+    double width = sqrt(pow((*r).p3->x - (*r).p1->x, 2) + pow((*r).p3->y - (*r).p1->y, 2));
+    printf("Area: %f\n",  length*width);
+    printf("Cimcunference: %f\n", 2*length+2*width);
+}
 
 int main()
 {
-    Point a = {3,4};
-    
-    printf("Wartośc x to %d, a y to %d\n", a.x, a.y);
-    Zmien(&a);
-    printf("Wartośc x to %d, a y to %d\n", a.x, a.y);
-    
-    Point b = {1,1};
-    Point c = {1,5};
-    Point d = {4,1};
-    Rectangle rec1 = {&a, &b, &c, &d};
-    
-    Pole_Obwod(&rec1);
-    
-    
-    return 0;
+    Point p = { 3,4 };
+    printf("x: %d\n", p.x);
+    printf("y: %d\n", p.y);
+    move(&p);
+    printf("x: %d\n", p.x);
+    printf("y: %d\n", p.y);
+    Point p2 = { 1,1 };
+    Point p3 = { 1,5 };
+    Point p4 = { 4,1 };
+    Rectangle r1 = { &p,&p2,&p3,&p4 };
+    areaAndCimcunference(&r1);
 }

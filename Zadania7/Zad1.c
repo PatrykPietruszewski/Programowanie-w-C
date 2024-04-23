99be279fc25fcbfb978c910c4ef4a19bd9fff56b
PatrykPietruszewski <147393955+PatrykPietruszewski@users.noreply.github.com>
2024-04-23 09:22:55 +0200

    Update Zad1.c

--- Zadania7/Zad1.c
+++ Zadania7/Zad1.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
+
+#define MAX_NUMBERS 20
+#define MAX_DIGITS 10
+
+int decode(char *line);
+int convert_to_int(char *number);
+int concat_int(int number1, int number2);
+
 int main()
 {
+    
+    
+    
     FILE *plik;
     plik = fopen("input.txt", "r");
 
     char linia[50];
+    char linia2[50];
     int suma = 0;
-    
+    int suma2 = 0;
     //CZĘŚĆ 1
     
      // Odczytujemy kolejne linie z pliku
@@ -43,11 +55,88 @@ int main()
         
         
     }
-    printf("Suma wartości energetycznych w naszym podręczniku to %d",suma);
-    
+    printf("Suma wartości energetycznych w naszym podręczniku to %d\n",suma);
+    fclose(plik);
     //CZĘŚĆ 2
     
-    fclose(plik);
+    FILE *plik2;
+    char line[100];
+
+    plik2 = fopen("input.txt", "r");
+
+
+    while (fgets(line, sizeof(line), plik2) != NULL) 
+    {
+        int energy = decode(line);
+        printf("%i\n", energy);
+        suma2 += energy;
+    }
+    printf("Suma wartości energetycznych w naszym podręczniku to %d\n", suma2);
+    fclose(plik2);
 
     return 0;
+}
+int decode(char *line) 
+{   
+    int count = 0;
+    char numbers[MAX_NUMBERS][MAX_DIGITS]; 
+
+    char* slowa[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int number_of_words = sizeof(slowa) / sizeof(slowa[0]);
+
+    // Przeszukiwanie ciągu znaków, by znaleźć podciągi lub cyfry
+    for (int i = 0; i < strlen(line); i++) 
+    {
+        if (isdigit(line[i])) 
+        {
+            char str[MAX_DIGITS]; 
+            sprintf(str, "%c", line[i]); 
+            strcpy(numbers[count], str); 
+            count++;
+        }
+        else 
+        {
+            for (int j = 0; j < number_of_words; j++) 
+            {
+                if (strncmp(&line[i], slowa[j], strlen(slowa[j])) == 0) 
+                {
+                    strncpy(numbers[count], &line[i], strlen(slowa[j])); 
+                    numbers[count][strlen(slowa[j])] = '\0';
+                    count++;
+                    i++; 
+                    break;
+                }
+            }
+        }
+    }
+    return concat_int(convert_to_int(numbers[0]), convert_to_int(numbers[count - 1]));
+}
+
+int convert_to_int(char *number) // zamiana slownych liczb na int
+{
+    char* slowa[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int number_of_words = sizeof(slowa) / sizeof(slowa[0]);
+    
+    for (int i = 0; i < number_of_words; ++i) 
+    {
+        if (strcmp(number, slowa[i]) == 0) 
+        {
+            return i; 
+        }
+    }
+    
+    return atoi(number); 
+}
+
+int concat_int(int number1, int number2) // konkatenacja dwoch liczb
+{
+    char str_number1[20]; 
+    char str_number2[20];
+    sprintf(str_number1, "%d", number1);
+    sprintf(str_number2, "%d", number2);
+
+    strcat(str_number1, str_number2);
+    int concatenated_number = atoi(str_number1);
+
+    return concatenated_number;
 }
